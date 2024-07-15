f2666ae57091b1e90f294976b068501a536094f0
ammu-17 <ammuannadurai17@gmail.com>
2024-07-15 11:07:09 +0530

    feature: using typescript , tailwind and daisy ui also. will implemene more

---
+++ app/component/AddtoCart.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from "react"
+
+export default function AddtoCart(){
+    return(
+        <div>
+              <button className="btn btn-primary" onClick={() => console.log('click')}>Add to cart</button>
+        </div>
+    )
+}
