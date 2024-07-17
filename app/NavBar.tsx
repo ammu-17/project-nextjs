a08314b43cbbdd48f5eab6dc4c8ffcd2d329baa4
ammu-17 <ammuannadurai17@gmail.com>
2024-07-17 17:00:18 +0530

    feature: authenctication with next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,8 @@ const NavBar = () => {
   return (
     <div className='flex bg-slate-200 p-3'>
       <Link href = '/' className='mr-5'>Next.js</Link>
-      <Link href='/users'>Users</Link>
+      <Link href='/users' className='mr-5'>Users</Link>
+      <Link href='/api/auth/signin'>Login</Link>
     </div>
   )
 }
