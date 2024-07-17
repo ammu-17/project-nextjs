667eca6b04803bf6e048ef189f009523b6ae4284
ammu-17 <ammuannadurai17@gmail.com>
2024-07-17 23:31:46 +0530

    feature: authentication with next auth, database adapters, session for client and server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./component/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1>Helloo {session && <span>{session.user!.name}</span>}</h1>
 
       <Link href = '/users'>users</Link>
       <ProductCard/>
